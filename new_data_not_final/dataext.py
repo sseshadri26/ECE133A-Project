de48a5bd49a92ca70f1d3a2834ef56c5d9667e7b
Sudarshan Seshadri <sudarshanseshadri26@gmail.com>
2023-03-16 16:48:31 -0700

    Started part 3

--- new_data_not_final/dataext.py
+++ new_data_not_final/dataext.py
@@ -2,27 +2,44 @@ import pandas as pd
 import time
 
 # Replace 'filename.csv' with the name of your file
-df = pd.read_csv('to_mod.csv')
+df = pd.read_csv('final_data_crashes.csv')
 
-# Convert start and end times to Unix time
-df.iloc[:, 1] = pd.to_datetime(df.iloc[:, 1], format='%Y-%m-%d %H:%M:%S').dt.tz_localize('UTC').dt.tz_convert('America/Los_Angeles').apply(lambda x: int(time.mktime(x.timetuple())))
+# # Convert start and end times to Unix time
+# df.iloc[:, 1] = pd.to_datetime(df.iloc[:, 1], format='%Y-%m-%d %H:%M:%S').dt.tz_localize('UTC').dt.tz_convert('America/Los_Angeles').apply(lambda x: int(time.mktime(x.timetuple())))
 
-df.iloc[:, 2] = pd.to_datetime(df.iloc[:, 2], format='%Y-%m-%d %H:%M:%S').dt.tz_localize('UTC').dt.tz_convert('America/Los_Angeles').apply(lambda x: int(time.mktime(x.timetuple())))
+# df.iloc[:, 2] = pd.to_datetime(df.iloc[:, 2], format='%Y-%m-%d %H:%M:%S').dt.tz_localize('UTC').dt.tz_convert('America/Los_Angeles').apply(lambda x: int(time.mktime(x.timetuple())))
 
-df.insert(4, 'duration', df.iloc[:, 2] - df.iloc[:, 1])
+# df.insert(4, 'duration', df.iloc[:, 2] - df.iloc[:, 1])
 
 
-# Add column for start time of the day in seconds after midnight
-df.insert(2, 'Seconds Since Start Of Day', df.iloc[:, 1] % 86400)
+# # Add column for start time of the day in seconds after midnight
+# df.insert(2, 'Seconds Since Start Of Day', df.iloc[:, 1] % 86400)
 
-# Add column for number of days since January 1st, 1970
-day_num = pd.to_datetime(df.iloc[:, 1], unit='s').dt.dayofyear - 1
+# # Add column for number of days since January 1st, 1970
+# day_num = pd.to_datetime(df.iloc[:, 1], unit='s').dt.dayofyear - 1
 
-# Insert day number as a new column
-df.insert(1, 'Day Number', day_num)
+# # Insert day number as a new column
+# df.insert(1, 'Day Number', day_num)
 
+# import time
+def getDay(epoch):
+    
+    # epoch = 1678153536
+    day = time.strftime('%A', time.localtime(epoch))
+    return day
+
+# put each entry in the col "StartTime" into the function getDay and put it in a new column "Day"
+df['Day'] = df['StartTime'].apply(getDay)
+
+one_hot = pd.get_dummies(df['Day'])
+
+# add the one-hot encoded columns to the original DataFrame
+df = pd.concat([df, one_hot], axis=1)
+
+# drop the original "Category" column
+df = df.drop('Day', axis=1)
 
 
 # Save the updated file
 # Replace 'new_filename.csv' with the name you want to give to the updated file
-df.to_csv('modded.csv', index=False)
+df.to_csv('modded111.csv', index=False)
